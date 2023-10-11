f65a7646d3602aac4c0fc6ae2ca2d9f0fc4c2ea5
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-11 19:50:34 +0530

    setup complete

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,14 +1,35 @@
 import * as THREE from 'three'
+import * as ZapparThree from '@zappar/zappar-threejs'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
+if (ZapparThree.browserIncompatible()) {
+    // The browserIncompatibleUI() function shows a full-page dialog that informs the user
+    // they're using an unsupported browser, and provides a button to 'copy' the current page
+    // URL so they can 'paste' it into the address bar of a compatible alternative.
+    ZapparThree.browserIncompatibleUI()
+
+    // If the browser is not compatible, we can avoid setting up the rest of the page
+    // so we throw an exception here.
+    throw new Error('Unsupported browser')
+}
+
 const scene = new THREE.Scene()
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
-
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+// Use this function to set your context
+ZapparThree.glContextSet(renderer.getContext())
+
+// Create a camera and set the scene background to the camera's backgroundTexture
+let camera = new ZapparThree.Camera()
+scene.background = camera.backgroundTexture
+
+// Request camera permissions and start the camera
+ZapparThree.permissionRequestUI().then((granted) => {
+    if (granted) camera.start()
+    else ZapparThree.permissionDeniedUI()
+})
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
@@ -23,8 +44,6 @@ scene.add(cube)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
@@ -34,7 +53,7 @@ function animate() {
 
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
-
+    camera.updateFrame(renderer)
     controls.update()
 
     render()
